b4bd36650264166929a9aa9267f6555c410228c4
riyanm24 <149254195+riyanm24@users.noreply.github.com>
2023-11-10 22:44:41 +0700

    Add files via upload
    perulangan while

---
+++ while.py
@@ -0,0 +1,8 @@
+num= 0 
+total= 0 
+while  num!=- 1 : 
+    num  =  int(input( 'Masukkan angka yang ingin ditambah atau masukkan -1 untuk berhenti:' )) 
+    if  num!=- 1 : 
+        total  =  total  +  num 
+print( ' Jumlah semua bilangan adalah:' ,total)
+	
